02a81d70968d3d657f571787806ddc04c87b4bf2
Sebas5158R <sebastianriveraa4@gmail.com>
2024-06-25 19:22:19 -0500

    feat: Add @radix-ui/react-slider and clsx dependencies, implement volume control in playerStore.js, and create Slider component

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,11 +1,13 @@
 import { Pause, Play } from "./Player";
 import { usePlayerStore } from "@/store/playerStore";
 
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size = 'small' }) {
     const { currentSong, isPlaying, setCurrentSong, setIsPlaying } = usePlayerStore(state => state);
 
+    const isPlayingPlayList = isPlaying && currentSong?.playlist.id === id;
+
     const handleClick = () => {
-        if(isPlaying) {
+        if(isPlayingPlayList) {
             setIsPlaying(false);
             return;
         }
@@ -19,11 +21,12 @@ export function CardPlayButton({ id }) {
             });
     }
 
-    const isPlayingPlayList = isPlaying && currentSong?.playlist.id === id;
+    const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-6 h-6';
+
 
     return (
-        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
-            {isPlayingPlayList ? <Pause /> : <Play />}
+        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400">
+            {isPlayingPlayList ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
         </button>
     );
 }
--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -5,6 +5,8 @@ import { create } from "zustand";
 export const usePlayerStore = create((set) => ({
     isPlaying: false, // - isPlaying: booleano que indica si la música está sonando
     currentSong: { playlist: null, song: null, songs: [] }, // - currentSong: objeto que contiene la información de la canción actual
+    volume: 1, // - volume: número que indica el volumen de la música
+    setVolume: (volume) => set({ volume }), // - setVolume: función que actualiza el volumen de la música
     setIsPlaying: (isPlaying) => set({ isPlaying }), // - setIsPlaying: función que actualiza el estado de reproducción
     setCurrentSong: (currentSong) => set({ currentSong }), // - setCurrentSong: función que actualiza la canción actual
 }));
